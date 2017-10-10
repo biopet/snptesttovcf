005636d900a618c727e99965d965fa0ff4f0b152
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/snptesttovcf/SnptestToVcfTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.snptesttovcf
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object SnptestToVcfTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
